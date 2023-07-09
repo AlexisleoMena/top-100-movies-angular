4a2fe547ea9e77b6fb1ccc4364465661af92460e
alexisleomena <alexismena2690@gmail.com>
2023-07-09 20:09:36 -0300

    Icono save v1

--- src/app/modules/movies/pages/saved-movies/saved-movies.component.ts
+++ src/app/modules/movies/pages/saved-movies/saved-movies.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from '../../interfaces/movie.interface';
 import { SavedMoviesService } from '../../services/saved-movies.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-saved-movies',
@@ -10,8 +11,21 @@ import { SavedMoviesService } from '../../services/saved-movies.service';
 export class SavedMoviesComponent implements OnInit {
   savedMovies: Movie[] = [];
 
-  constructor(private savedMoviesService: SavedMoviesService){}
+  constructor(
+    private savedMoviesService: SavedMoviesService,
+    private _snackBar: MatSnackBar
+  ) {}
   ngOnInit(): void {
     this.savedMovies = this.savedMoviesService.savedMovies;
+    this.savedMoviesService.setComponentRef(this);
+  }
+
+  openSnackBar() {
+    this._snackBar.open(this.savedMoviesService.message, 'Close', {
+      horizontalPosition: 'start',
+      verticalPosition: 'bottom',
+      duration: 2000,
+      panelClass: 'snackbar-button',
+    });
   }
 }
--- src/app/modules/movies/services/saved-movies.service.ts
+++ src/app/modules/movies/services/saved-movies.service.ts
@@ -1,32 +1,53 @@
 import { Injectable } from '@angular/core';
 import { Movie } from '../interfaces/movie.interface';
+import { SavedMoviesComponent } from '../pages/saved-movies/saved-movies.component';
+import { ListMoviesComponent } from '../pages/list-movies/list-movies.component';
+import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SavedMoviesService {
   savedMovies: Movie[] = [];
-
+  message: string = '';
+  isLoading: boolean = false;
+  componentRef: SavedMoviesComponent | ListMoviesComponent | null = null;
+  savedMoviesChanged = new Subject<void>();
+  
   constructor() {
     const savedMoviesString = localStorage.getItem('peliculas');
     if (savedMoviesString) {
       this.savedMovies = JSON.parse(savedMoviesString);
     }
   }
-  saveMovie(movie: Movie) {
-    const index = this.savedMovies.findIndex((savedMovie) => savedMovie.id === movie.id);
 
-    if (index > -1) {
-      this.savedMovies.splice(index, 1);
-      console.log('Película removida del array de películas guardadas');
-    } else {
-      this.savedMovies.push(movie);
-      console.log('Película guardada correctamente en el array de películas guardadas');
-    }
-
-    this.saveInLocalStorage();
+  setComponentRef(componentRef: SavedMoviesComponent| ListMoviesComponent) {
+    this.componentRef = componentRef;
   }
-  isMovieSaved(movie: Movie): boolean{
+
+  saveMovie(movie: Movie) {
+    this.isLoading = true;
+    setTimeout(() => {
+      const index = this.savedMovies.findIndex(
+        (savedMovie) => savedMovie.id === movie.id
+        );
+        
+        if (index > -1) {
+          this.savedMovies.splice(index, 1);
+          this.message = 'Movie removed!';
+        } else {
+          this.savedMovies.push(movie);
+          this.message = 'Movie saved!';
+        }
+        this.savedMoviesChanged.next();
+        this.saveInLocalStorage();
+        if (this.componentRef) {
+          this.componentRef.openSnackBar();
+        }
+        this.isLoading = false;
+      }, 1000)
+  }
+  isMovieSaved(movie: Movie): boolean {
     return this.savedMovies.some((savedMovie) => savedMovie.id === movie.id);
   }
   private saveInLocalStorage() {
